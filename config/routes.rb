53cda38e272f1a270ca9a596a97eafc9e60871d4
paperplanegirl <ibon.chia@gmail.com>
2016-10-06 18:44:20 +0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   delete "delete/:id" => "users#destroy"
 
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
